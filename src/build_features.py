275cffc451e8eac1a0ac152e2da0ec4cc7967c23
test <damianlukawski11@gmail.com>
2020-09-02 09:35:03 +0200

    cleaning build_feature.py

--- src/build_features.py
+++ src/build_features.py
@@ -4,20 +4,21 @@ import sklearn
 
 def build_features(input_file):
 
-    df = pd.read_csv(input_file)
+    df = pd.read_csv(input_file,  sep = ";")
 
     #change 'male' and 'female' to 0 and 1 for numeric calculations
-    df["Sex"] = df["Sex"].replace("male", 0)
-    df["Sex"] = df["Sex"].replace("female", 1)
+    df.replace({'Sex': {"male": 0,"female": 1}} )
+    #df["Sex"].replace("female", 1)
 
 
 
-    
-    for i in embarked_dict.keys():
-        df["Embarked"].replace(i, embarked_dict[i], inplace = True)
 
-    df["FamilySize"] = df["SibSp"] + df["Parch"] + 1
+
+    df['FamilySize'] = df['SibSp'] + df['Parch']+1
     df["IsAlone"] = 0
     df.loc[df["FamilySize"] == 1, "IsAlone"] = 1
     output_file = input_file.split('.')[0] + "_bf.csv"
-    df.to_csv(output_file)
+    df.to_csv(output_file,  sep = ";")
+
+
+
